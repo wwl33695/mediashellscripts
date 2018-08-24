153dbfd955c9578ac43cb8d9e5fae430a52b4ec1
wwl33695 <wwl33695@126.com>
2018-08-24 17:05:16 +0800

    mix audio and moving drawtext

--- moving_drawtext.sh
+++ moving_drawtext.sh
@@ -1,1 +1,3 @@
-ffplay test.mp4 -vf "drawtext=text='drawtext':x=mod(n*4\,w):y=100"
+ffplay test.mp4 -vf "drawtext=text='drawtext':x=mod(n*4\,w+tw)-tw:y=100"
+
+# ffplay test.mp4 -vf "drawtext=text='drawtext':x=w-mod(n*4\,w+tw):y=100"
