9244745359f944a78919516fd2e3bb93e715f51b
wangwl <wangwl@dilusense.com>
2019-07-23 13:35:32 +0800

    modify frag

--- fragment mp4.sh
+++ fragment mp4.sh
@@ -1,2 +1,2 @@
 #fmp4 used in html5
-ffmpeg -i 1.mp4 -c copy -movflags frag_keyframe+separate_moof+faststart+empty_moov+omit_tfhd_offset frag.mp4
+ffmpeg -i 1.mp4 -c copy -movflags empty_moov+frag_keyframe+separate_moof+omit_tfhd_offset frag.mp4
