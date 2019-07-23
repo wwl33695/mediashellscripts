a90eaa6544ca8e7413fa0ba1d2176e437164ab85
wangwl <wangwl@dilusense.com>
2019-07-23 10:37:31 +0800

    modify

--- capture_image_every_x_frames_from_movie.sh
+++ capture_image_every_x_frames_from_movie.sh
@@ -1,1 +1,4 @@
 ffmpeg -i test.mp4 -vf select='not(mod(n\,1000))' -vsync 0 vframes/%d.jpg
+
+#从某个时间起输出N张图片
+ffmpeg -i test.mp4 -ss 00:00:14.435 -vframes N vframes/%d.jpg
--- capture_image_every_x_seconds_from_movie.sh
+++ capture_image_every_x_seconds_from_movie.sh
@@ -1,1 +1,7 @@
 ffmpeg -i test.mp4 -vf select='isnan(prev_selected_t)+gte(t-prev_selected_t\,10)' -vsync 0 vframes/%d.jpg
+
+#每一分钟输出一张图片
+ffmpeg -i test.mp4 -vf fps=1/60 vframes/%d.jpg
+
+#每十分钟输出一张图片
+ffmpeg -i test.mp4 -vf fps=1/600 vframes/%d.jpg
